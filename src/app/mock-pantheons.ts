a0f0a37a80e162ae635790f9381387463a86fa39
fetuch <rafal.nowak@punkgate.pl>
2022-05-18 14:56:51 +0200

    Mock pantheons data

---
+++ src/app/mock-pantheons.ts
@@ -0,0 +1,21 @@
+import { Pantheon } from './pantheon';
+const baseImgUrl = '/assets/pantheons/';
+
+export const PANTHEONS: Pantheon[] = [
+  {
+    name: 'Egyptian',
+    banner: baseImgUrl + 'egyptian.jpg',
+  },
+  {
+    name: 'Greek',
+    banner: baseImgUrl + 'greek.jpg',
+  },
+  {
+    name: 'Hindu',
+    banner: baseImgUrl + 'hindu.jpg',
+  },
+  {
+    name: 'Nordic',
+    banner: baseImgUrl + 'nordic.jpg',
+  },
+];
