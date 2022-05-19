8988b37e9e79869deb7c64689178071229b4a13a
fetuch <rafal.nowak@punkgate.pl>
2022-05-19 23:35:31 +0200

    Add battle logic

--- src/app/battle.service.ts
+++ src/app/battle.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BattleLogService } from './battle-log.service';
+import { Champion } from './champion';
 import { Pantheon } from './pantheon';
 import { Team } from './team';
 
@@ -7,6 +8,9 @@ import { Team } from './team';
   providedIn: 'root',
 })
 export class BattleService {
+  opponents?: Team[];
+  rounds: number = 10;
+
   constructor(private battleLogService: BattleLogService) {}
 
   createTemporaryTeam(pantheon: Pantheon): Team {
@@ -14,9 +18,7 @@ export class BattleService {
       .champions!.sort(() => Math.random() - 0.5)
       .slice(0, 3);
 
-    this.battleLogService.add(
-      'BattleService: Opponent team has been selected.'
-    );
+    this.battleLogService.add('Opponent team has been selected.');
 
     return { members: team } as Team;
   }
@@ -31,7 +33,75 @@ export class BattleService {
     return pantheons[Math.floor(Math.random() * pantheons.length)];
   }
 
-  start(opponents: Team[] | undefined): void {
-    this.battleLogService.add('BattleService: So it has begun!');
+  start(opponents: Team[]): void {
+    this.opponents = opponents;
+    this.battleLogService.add('So it has begun!');
+
+    while (this.rounds > 0 && this.areTeamsAlive()) {
+      this.battleLogService.add('Round');
+      this.rounds--;
+      this.playARound();
+    }
+
+    this.battleLogService.add('Finish');
+
+    const summary = this.opponents!.reduce(
+      (total: Champion[], current: Team) => [...total, ...current.members],
+      []
+    ).forEach((champion) => {
+      this.battleLogService.add(
+        `${champion.name} [${champion.pantheon}] HP ${champion.hp}`
+      );
+    });
+  }
+
+  playARound(): void {
+    // determine the order of the movements of each individual champion
+    const champions = this.orderChampions();
+
+    champions.forEach((champion) => {
+      const opponent = this.pickLivingOpponent(
+        champions.filter((member) => member.pantheon !== champion.pantheon)
+      );
+
+      if (opponent) {
+        opponent.hp -= champion.attack;
+
+        this.battleLogService.add(
+          `${champion.name} [${champion.pantheon}] damages ${opponent.name} [${opponent.pantheon}] for ${champion.attack}hp, Current ${opponent.name}'s HP: ${opponent.hp}`
+        );
+      }
+    });
+  }
+
+  /**
+   * Determines the order of movements.
+   * Returns randomly sorted lifing champions.
+   */
+  orderChampions(): Champion[] {
+    const list = this.opponents!.reduce(
+      (total: Champion[], current: Team) => [...total, ...current.members],
+      []
+    )
+      .filter((champion) => champion.hp > 0)
+      .sort(() => Math.random() - 0.5);
+
+    return list;
+  }
+
+  /** Checks if each team has at least one member with more than 0hp */
+  areTeamsAlive(): boolean {
+    return this.opponents!.every((team) =>
+      team.members.some((member) => member.hp > 0)
+    );
+  }
+
+  /** Randomly pick living opponent */
+  pickLivingOpponent(champions: Champion[]): Champion | null {
+    return (
+      champions
+        .sort(() => Math.random() - 0.5)
+        .find((champion) => champion.hp > 0) ?? null
+    );
   }
 }
