06a36d2c8f6be38773643c9f06072556805a1aff
fetuch <rafal.nowak@punkgate.pl>
2022-05-27 13:58:57 +0200

    Build

--- src/app/feature-list/feature-list.component.ts
+++ src/app/feature-list/feature-list.component.ts
@@ -35,7 +35,7 @@ export class FeatureListComponent implements OnInit {
     {
       name: 'Tests',
       description:
-        'E2e and unit test to assure that the flow of an application is performing as designed from start to finish.',
+        'Assure that the flow of an application is performing as designed from start to finish.',
     },
     {
       name: 'Forms',
