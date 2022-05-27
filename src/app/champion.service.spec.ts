0be1ba870807e079e336bb69e3c654dd1686f92f
fetuch <rafal.nowak@punkgate.pl>
2022-05-27 10:37:32 +0200

    Test champion service

--- src/app/champion.service.spec.ts
+++ src/app/champion.service.spec.ts
@@ -1,16 +1,221 @@
-import { TestBed } from '@angular/core/testing';
+// Http testing module and mocking controller
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
+import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+
+import { Champion } from './champion';
 import { ChampionService } from './champion.service';
 
 describe('ChampionService', () => {
-  let service: ChampionService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let championService: ChampionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ChampionService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    championService = TestBed.inject(ChampionService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(championService).toBeTruthy();
+  });
+
+  describe('#getChampions', () => {
+    const expectedChampions: Champion[] = [
+      { id: 1, name: 'Apophis' },
+      { id: 2, name: 'Thor' },
+    ] as Champion[];
+
+    it('should return expected champions', () => {
+      championService.getChampions().subscribe((champions) => {
+        expect(champions.length).toBe(2);
+        expect(champions).toEqual(expectedChampions);
+      });
+
+      const req = httpTestingController.expectOne(championService.championsUrl);
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(expectedChampions);
+    });
+
+    it('should be OK returning no champions', () => {
+      championService
+        .getChampions()
+        .subscribe((champions) => expect(champions.length).toEqual(0));
+
+      const req = httpTestingController.expectOne(championService.championsUrl);
+      req.flush([]); // Respond with no champions
+    });
+
+    it('should turn 404 into an empty champions result', () => {
+      championService
+        .getChampions()
+        .subscribe((champions) => expect(champions.length).toEqual(0));
+
+      const req = httpTestingController.expectOne(championService.championsUrl);
+
+      // respond with a 404 and the error message in the body
+      const msg = 'deliberate 404 error';
+      req.flush(msg, { status: 404, statusText: 'Not Found' });
+    });
+  });
+
+  describe('#getChampion', () => {
+    const expectedChampion: Champion = { id: 2, name: 'Thor' } as Champion;
+
+    it('should return expected champion', () => {
+      championService
+        .getChampion(1)
+        .subscribe((champion) => expect(champion).toEqual(expectedChampion));
+
+      const req = httpTestingController.expectOne(
+        `${championService.championsUrl}/1`
+      );
+
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(expectedChampion);
+    });
+  });
+
+  describe('#addChampion', () => {
+    it('should save a champion and return it', () => {
+      const champion: Champion = { name: 'A' } as Champion;
+
+      championService
+        .addChampion(champion)
+        .subscribe((data) => expect(data).toEqual(champion));
+
+      const req = httpTestingController.expectOne(championService.championsUrl);
+      expect(req.request.method).toEqual('POST');
+      expect(req.request.body).toEqual(champion);
+
+      const expectedResponse = new HttpResponse({
+        status: 200,
+        statusText: 'OK',
+        body: champion,
+      });
+
+      req.event(expectedResponse);
+    });
+
+    it('should turn 404 error into return of the added champion', () => {
+      const champion: Champion = { name: 'A' } as Champion;
+
+      championService
+        .addChampion(champion)
+        .subscribe((data) => expect(data).toEqual(champion));
+
+      const req = httpTestingController.expectOne(championService.championsUrl);
+
+      const msg = 'deliberate 404 error';
+      req.flush(msg, { status: 404, statusText: 'Not Found' });
+    });
+  });
+
+  describe('#updateChampion', () => {
+    it('should update a champion and return it', () => {
+      const updateChampion: Champion = { id: 1, name: 'A' } as Champion;
+
+      championService
+        .updateChampion(updateChampion)
+        .subscribe((data) => expect(data).toEqual(updateChampion));
+
+      const req = httpTestingController.expectOne(championService.championsUrl);
+      expect(req.request.method).toEqual('PUT');
+      expect(req.request.body).toEqual(updateChampion);
+
+      const expectedResponse = new HttpResponse({
+        status: 200,
+        statusText: 'OK',
+        body: updateChampion,
+      });
+
+      req.event(expectedResponse);
+    });
+
+    it('should turn 404 error into return of the update champion', () => {
+      const updateChampion: Champion = { id: 1, name: 'A' } as Champion;
+
+      championService
+        .updateChampion(updateChampion)
+        .subscribe((data) => expect(data).toEqual(updateChampion));
+
+      const req = httpTestingController.expectOne(championService.championsUrl);
+
+      const msg = 'deliberate 404 error';
+      req.flush(msg, { status: 404, statusText: 'Not Found' });
+    });
+  });
+
+  describe('#deleteChampion', () => {
+    let champion: Champion;
+
+    beforeEach(() => {
+      champion = { id: 1, name: 'Apophis' } as Champion;
+    });
+
+    it('should remove champion from champions list', () => {
+      const url = `${championService.championsUrl}/1`;
+
+      championService
+        .deleteChampion(1)
+        .subscribe((data) => expect(data).toEqual(champion));
+
+      const req = httpTestingController.expectOne(url);
+      expect(req.request.method).toEqual('DELETE');
+
+      req.flush(champion);
+    });
+  });
+
+  describe('searchChampions', () => {
+    const expectedChampions: Champion[] = [
+      { id: 2, name: 'Thor' },
+      { id: 3, name: 'Thot' },
+    ] as Champion[];
+
+    const term: string = 'ho';
+
+    it('should return expected champions', () => {
+      championService.searchChampions(term).subscribe((champions) => {
+        expect(champions.length).toBe(2);
+        expect(champions).toEqual(expectedChampions);
+      });
+
+      const req = httpTestingController.expectOne(
+        `${championService.championsUrl}/?name=${term}`
+      );
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(expectedChampions);
+    });
+
+    it('should turn 404 into an empty champions result', () => {
+      championService
+        .searchChampions(term)
+        .subscribe((champions) => expect(champions.length).toEqual(0));
+
+      const req = httpTestingController.expectOne(
+        `${championService.championsUrl}/?name=${term}`
+      );
+
+      // respond with a 404 and the error message in the body
+      const msg = 'deliberate 404 error';
+      req.flush(msg, { status: 404, statusText: 'Not Found' });
+    });
   });
 });
--- src/app/champion.service.ts
+++ src/app/champion.service.ts
@@ -1,55 +1,68 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
+import {
+  HandleError,
+  HttpErrorHandlerService,
+} from './http-error-handler.service';
+
 import { Champion } from './champion';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChampionService {
-  private championsUrl = 'api/champions';
+  championsUrl = 'api/champions';
+  private handleError: HandleError;
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    httpErrorHandler: HttpErrorHandlerService
+  ) {
+    this.handleError = httpErrorHandler.createHandleError('ChampionService');
+  }
 
   getChampions(): Observable<Champion[]> {
-    return this.http.get<Champion[]>(this.championsUrl);
+    return this.http
+      .get<Champion[]>(this.championsUrl)
+      .pipe(catchError(this.handleError<Champion[]>('getChampions', [])));
   }
 
   getChampion(id: number): Observable<Champion> {
     const url = `${this.championsUrl}/${id}`;
 
-    return this.http.get<Champion>(url);
+    return this.http
+      .get<Champion>(url)
+      .pipe(catchError(this.handleError<Champion>('getChampion')));
   }
 
-  /** POST: add a new champion to the server */
   addChampion(champion: Champion): Observable<Champion> {
-    return this.http.post<Champion>(
-      this.championsUrl,
-      champion,
-      this.httpOptions
-    );
+    return this.http
+      .post<Champion>(this.championsUrl, champion, this.httpOptions)
+      .pipe(catchError(this.handleError<Champion>('addChampion', champion)));
   }
 
-  /** PUT: update the champion on the server */
   updateChampion(champion: Champion): Observable<any> {
-    return this.http.put(this.championsUrl, champion, this.httpOptions);
+    return this.http
+      .put(this.championsUrl, champion, this.httpOptions)
+      .pipe(catchError(this.handleError<any>('updateChampion', champion)));
   }
 
-  /** DELETE: delete the champion from the server */
   deleteChampion(id: number): Observable<Champion> {
     const url = `${this.championsUrl}/${id}`;
 
-    return this.http.delete<Champion>(url, this.httpOptions);
+    return this.http
+      .delete<Champion>(url, this.httpOptions)
+      .pipe(catchError(this.handleError<Champion>('deleteChampion')));
   }
 
-  /* GET champions whose name contains search term */
   searchChampions(term: string): Observable<Champion[]> {
-    return this.http.get<Champion[]>(
-      `${this.championsUrl}/?name=${term.trim()}`
-    );
+    return this.http
+      .get<Champion[]>(`${this.championsUrl}/?name=${term.trim()}`)
+      .pipe(catchError(this.handleError<Champion[]>('searchChampions', [])));
   }
 }
