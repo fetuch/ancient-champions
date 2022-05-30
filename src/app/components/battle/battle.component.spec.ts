9de2b76a70cb6a75cd05d383d3a764fc59e7f659
fetuch <rafal.nowak@punkgate.pl>
2022-05-30 12:52:49 +0200

    Rafactor battle stage

--- src/app/components/battle/battle.component.spec.ts
+++ src/app/components/battle/battle.component.spec.ts
@@ -2,41 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { BattleComponent } from './battle.component';
-import { PantheonService } from '../../services/pantheon.service';
-import { TeamService } from '../../services/team.service';
-import { ChampionService } from '../../services/champion.service';
 import { BattleLogService } from '../../services/battle-log.service';
 import { BattleService } from '../../services/battle.service';
-import { of } from 'rxjs';
 
 describe('BattleComponent', () => {
   let component: BattleComponent;
   let fixture: ComponentFixture<BattleComponent>;
-  let fakePantheonService: PantheonService;
-  let fakeTeamService: TeamService;
-  let fakeChampionService: ChampionService;
   let fakeBattleLogService: BattleLogService;
   let fakeBattleService: BattleService;
 
   beforeEach(async () => {
-    fakeTeamService = jasmine.createSpyObj<TeamService>('TeamService', {
-      getTeam: of(),
-    });
-
-    fakeChampionService = jasmine.createSpyObj<ChampionService>(
-      'ChampionService',
-      {
-        getChampions: of([]),
-      }
-    );
-
-    fakePantheonService = jasmine.createSpyObj<PantheonService>(
-      'PantheonService',
-      {
-        getPantheon: of(),
-      }
-    );
-
     fakeBattleLogService = jasmine.createSpyObj<BattleLogService>(
       'BattleLogService',
       {
@@ -45,16 +20,14 @@ describe('BattleComponent', () => {
     );
 
     fakeBattleService = jasmine.createSpyObj<BattleService>('BattleService', {
-      getRandomPantheon: undefined,
+      getOpponents: undefined,
+      prepareOpponents: undefined,
     });
 
     await TestBed.configureTestingModule({
       declarations: [BattleComponent],
       imports: [RouterTestingModule],
       providers: [
-        { provide: PantheonService, useValue: fakePantheonService },
-        { provide: TeamService, useValue: fakeTeamService },
-        { provide: ChampionService, useValue: fakeChampionService },
         { provide: BattleLogService, useValue: fakeBattleLogService },
         { provide: BattleService, useValue: fakeBattleService },
       ],
