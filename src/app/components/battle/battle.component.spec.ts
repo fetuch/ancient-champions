e4320f3ceb45a9f2bcb1a33e32c223bf65fe59e4
fetuch <rafal.nowak@punkgate.pl>
2022-05-30 13:12:04 +0200

    Build

--- src/app/components/battle/battle.component.spec.ts
+++ src/app/components/battle/battle.component.spec.ts
@@ -2,16 +2,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { BattleComponent } from './battle.component';
+import { TeamService } from '../../services/team.service';
 import { BattleLogService } from '../../services/battle-log.service';
 import { BattleService } from '../../services/battle.service';
+import { of } from 'rxjs';
 
 describe('BattleComponent', () => {
   let component: BattleComponent;
   let fixture: ComponentFixture<BattleComponent>;
+  let fakeTeamService: TeamService;
   let fakeBattleLogService: BattleLogService;
   let fakeBattleService: BattleService;
 
   beforeEach(async () => {
+    fakeTeamService = jasmine.createSpyObj<TeamService>('TeamService', {
+      getTeam: of(),
+    });
+
     fakeBattleLogService = jasmine.createSpyObj<BattleLogService>(
       'BattleLogService',
       {
@@ -28,6 +35,7 @@ describe('BattleComponent', () => {
       declarations: [BattleComponent],
       imports: [RouterTestingModule],
       providers: [
+        { provide: TeamService, useValue: fakeTeamService },
         { provide: BattleLogService, useValue: fakeBattleLogService },
         { provide: BattleService, useValue: fakeBattleService },
       ],
